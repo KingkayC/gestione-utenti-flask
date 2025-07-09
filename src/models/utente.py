954e09fba2a60bc680aa52d4cdd6af5c68dfaee9
Claudio C <claudio.castelletto@gmail.com>
2025-07-09 21:09:14 +0200

    Update utente.py

--- src/models/utente.py
+++ src/models/utente.py
@@ -1,5 +1,10 @@
-from src.main import db
+from src import db
 
 class Utente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    cognome = db.Column(db.String(50), nullable=False)
     codice_fiscale = db.Column(db.String(16), unique=True, nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
